7fc398e421a51d45b5aa672af8520565fae1da8a
dotuantai <dotuantai.dev@gmail.com>
2026-07-18 14:30:20 +0700

    Revert "Merge pull request #14 from dotuantai/migration/net10"
    This reverts commit 4212dd282b890d8056c3bab44822eff10e63506c, reversing
    changes made to 85decb267274bc1e5872726d475dec50d2bde504.

--- API_v2/Models/DTOs/LoginResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace API_v2.Models.DTOs
-{
-    public class LoginResponse
-    {
-        public string AccessToken { get; set; } = string.Empty;
-        public string? RefreshToken { get; set; }
-    }
-}
--- API_v2/Models/DTOs/UserSearchResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace API_v2.Models.DTOs
-{
-    public class UserSearchResponse
-    {
-        public Guid UserId { get; set; }
-        public string Email { get; set; } = string.Empty;
-    }
-}
